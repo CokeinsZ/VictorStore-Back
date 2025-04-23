9cf279a34171346f4495fdd8d7f59de58a0c412a
Stiven Alejandro Carvajal Perez <stivena.carvajalp@autonoma.edu.co>
2025-04-23 01:18:00 -0500

    Eliminacion de los filtros de seguridad
    Con fines de pruebas
    Se quiere comprobar los certificados ssl generados por Azure

--- API/src/main.ts
+++ API/src/main.ts
@@ -6,43 +6,7 @@ import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: readFileSync('./secrets/key.pem'),
-    cert: readFileSync('./secrets/cert.pem'),
-    passphrase: process.env.PRIVATE_KEY_PASSPHRASE,
-  };
-
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions,
-  });
-
-  // Configuración básica de Helmet
-  app.use(
-    helmet({
-      contentSecurityPolicy: {
-        directives: {
-          defaultSrc: [`'self'`],
-          styleSrc: [`'self'`],
-          imgSrc: [`'self'`, 'data:' ],
-          scriptSrc: [`'self'` ],
-        },
-      },
-      hsts: { maxAge: 31536000 },
-      frameguard: { action: 'deny' },
-    })
-  );
-
-  // Configuración de CORS
-  app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  });
-
-  const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT') || 443);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
